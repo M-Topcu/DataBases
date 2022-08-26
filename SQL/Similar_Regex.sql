c28bd5ebc2c0b896908de2e62428a7ad7ebfd228
M-Topcu <mamitopcu35@gmail.com>
2022-08-26 17:13:49 +0200

    Update

--- SQL/Similar_Regex.sql
+++ SQL/Similar_Regex.sql
@@ -43,3 +43,44 @@ select * from kelimeler where kelime ~* 'ot' or kelime ~* 'at';
 -- 'ho' veya 'hi' ile başlayan kelimeleri listeleyeniz
 select * from kelimeler where kelime SIMILAR TO '(ho|Ho|hO|HO|hi|HI|Hi|hI)%';
 select * from kelimeler where kelime ilike 'ho%' or kelime ilike 'hi%';
+select * from kelimeler where kelime ~* 'h[oi](.*)' -- Regexte . bir karakteri temsil eder, koseli parantez de tek karakter icin secenekler
+
+--Sonu 't' veya 'm' ile bitenleri listeleyeniz
+select * from kelimeler where kelime SIMILAR TO '%t|%m|%M|%T';
+select * from kelimeler where kelime ~~* '%t' or kelime ~~* '%m';
+select * from kelimeler where kelime ~* '(.*)[mt]$'; -- $ bitisi gosterir
+
+--h ile başlayıp t ile biten 3 harfli kelimeleri listeleyeniz
+select kelime from kelimeler where kelime SIMILAR TO '(H|h)[a-z, A-Z, 0-9](t|T)';
+select kelime from kelimeler where kelime ~~* 'h_t';
+select kelime from kelimeler where kelime ~* 'h(.)t$';
+
+--İlk karakteri 'h', son karakteri 't' ve ikinci karakteri 'a'dan 'e'ye herhangi bir karakter olan 
+--“kelime" değerlerini çağırın.
+SELECT kelime from kelimeler where kelime SIMILAR TO '(h|H)[a-e,A-E]%(T|t)';
+select kelime from kelimeler where kelime ~* 'h[a-e,](.*)t$';
+
+--İlk karakteri 's', 'a' veya 'y' olan "kelime" değerlerini çağırın.
+select kelime from kelimeler where kelime ~* '^[say](.*)'; -- ^ baslangici temsil eder
+
+--Son karakteri 'm', 'a' veya 'f' olan "kelime" değerlerini çağırın.
+select kelime from kelimeler where kelime ~* '[maf]$';
+
+-- İlk harfi h, son harfi t olup 2.harfi a veya i olan 3 harfli kelimelerin tüm bilgilerini sorgulayalım.
+select * from kelimeler where kelime similar to 'h[a|i]t';
+select * from kelimeler where kelime ~ '^h[a|i]t$';
+
+--İlk harfi 'b' dan ‘s' ye bir karakter olan ve ikinci harfi herhangi bir karakter olup 
+--üçüncü harfi ‘l' olan “kelime" değerlerini çağırın. 
+select kelime from kelimeler WHERE kelime ~ '^[b-s].l(.*)';
+
+--içerisinde en az 2 adet o o barıdıran kelimelerin tüm bilgilerini listeleyiniz.
+select * from kelimeler where kelime similar to '%[oO][oO]%';
+select * from kelimeler where kelime similar to '%[oO]{2}%'; -- Süslü parantez içinde belirttiğimiz rakam bir önceki 
+                                                           --köşeli parantez içinde kaçtane olduğunu belirtir
+
+--içerisinde en az 4 adet oooo barıdıran kelimelerin tüm bilgilerini listeleyiniz.
+select * from kelimeler where kelime similar to '%[oO]{4}%'; -- yan yana 4 tane
+
+--'a', 's' yada 'y' ile başlayan VE 'm' yada 'f' ile biten "kelime" değerlerini çağırın.
+select kelime from kelimeler where kelime ~* '^[a|s|y](.*)[m|f]$';
